ccf955a18ad332eadba03720eacb016595a863b6
vinhlt <vinhlt@intern.vn>
2024-04-14 18:27:59 +1000

    add bottom sheet restaurant

--- app/src/main/java/com/example/gourmetcompass/ui_restaurant_detail/RestaurantDetailActivity.java
+++ app/src/main/java/com/example/gourmetcompass/ui_restaurant_detail/RestaurantDetailActivity.java
@@ -1,13 +1,24 @@
 package com.example.gourmetcompass.ui_restaurant_detail;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.gourmetcompass.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Objects;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
 
@@ -15,6 +26,8 @@ public class RestaurantDetailActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     private final String[] titles = {"Detail", "Menu", "Gallery", "Review"};
+
+    ImageButton plusBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,27 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> tab.setText(titles[position])).attach();
 
 
+        // Open bottom sheet
+        plusBtn = findViewById(R.id.btn_plus);
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_restaurant);
+        dialog.show();
+        if (dialog.getWindow() != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+        }
     }
 
 }
