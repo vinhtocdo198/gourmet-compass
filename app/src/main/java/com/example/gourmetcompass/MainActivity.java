889be9dabf4f0821d83872063514a98833b7db2a
vinhlt <vinhlt@intern.vn>
2024-04-16 03:39:59 +1000

    add map activity

--- app/src/main/java/com/example/gourmetcompass/MainActivity.java
+++ app/src/main/java/com/example/gourmetcompass/MainActivity.java
@@ -3,49 +3,27 @@ package com.example.gourmetcompass;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.gourmetcompass.ui_restaurant_detail.RestaurantDetailActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.gourmetcompass.databinding.ActivityMainBinding;
+import com.example.gourmetcompass.ui_restaurant_detail.RestaurantDetailActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
     Button btn;
+    Button mapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
 
         // Create a new Button
         btn = findViewById(R.id.button5);
-//        Button button = new Button(this);
+        mapBtn = findViewById(R.id.map_btn);
+
         btn.setText("Go to Another Screen");
 
-        // Set an OnClickListener for the Button
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (btn.getParent() != null) {
-            ((ViewGroup) btn.getParent()).removeView(btn);
-        }
-
-        // Set the Button as the content view of the activity
-        setContentView(btn);
-
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the AnotherActivity when the Button is clicked
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/gourmetcompass/MapsActivity.java
@@ -0,0 +1,40 @@
+package com.example.gourmetcompass;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        map = findViewById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+        LatLng loc = new LatLng(14.0583, 108.2772);
+        gMap.addMarker(new MarkerOptions().position(loc).title("Vietnam"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 12));
+    }
+}
