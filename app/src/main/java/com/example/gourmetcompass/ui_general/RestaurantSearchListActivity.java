f97b6669876dad67d01409b20b76f32b39e64fe3
vinhlt <vinhlt@intern.vn>
2024-05-02 23:27:37 +1000

    temp

--- app/src/main/java/com/example/gourmetcompass/ui_general/RestaurantSearchListActivity.java
+++ app/src/main/java/com/example/gourmetcompass/ui_general/RestaurantSearchListActivity.java
@@ -8,6 +8,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.example.gourmetcompass.R;
 
@@ -30,9 +31,6 @@ public class RestaurantSearchListActivity extends AppCompatActivity {
         // Init views
         initViews();
 
-        // Set search text result and quantity
-        searchTextResult.setText(searchQuery);
-
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +38,13 @@ public class RestaurantSearchListActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.stay_still, R.anim.slide_out);
             }
         });
+
+        // Set search text result and quantity
+        searchTextResult.setText(searchQuery);
+        resultQuantity.setText(String.format(getApplicationContext().getString(R.string.n_results), 300)); // TODO: change 300
+
+//        setupFilterBtn(highestRatedBtn);
+
     }
 
     private void initViews() {
@@ -49,7 +54,27 @@ public class RestaurantSearchListActivity extends AppCompatActivity {
         mostReviewedBtn = findViewById(R.id.most_reviewed_filter);
         nearestBtn = findViewById(R.id.nearest_filter);
         backBtn = findViewById(R.id.btn_back_res_search_list);
-        progressBar = findViewById(R.id.res_search_list_progress_bar);
+//        progressBar = findViewById(R.id.res_search_list_progress_bar);
+    }
+
+    private void setupFilterBtn(Button button) {
+        // Initially, the button is not clicked
+        button.setTag(false);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isClicked = (boolean) button.getTag();
+                if (isClicked) {
+                    // If the button was previously clicked, reset its background and update its state
+                    button.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_star_filter, null));
+                    button.setTag(false);
+                } else {
+                    // If the button was not previously clicked, change its background and update its state
+                    button.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_star_filter_full, null));
+                    button.setTag(true);
+                }
+            }
+        });
     }
 
 }
